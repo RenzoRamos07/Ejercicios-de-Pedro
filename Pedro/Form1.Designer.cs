288400880cb0daf9e132124046d88c1497d2276b
RenzoRamos07 <102114393+RenzoRamos07@users.noreply.github.com>
2023-09-02 00:46:53 -0500

    Add files via upload

---
+++ Pedro/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Pedro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_producto1 = new System.Windows.Forms.CheckBox();
+            this.cb_producto2 = new System.Windows.Forms.CheckBox();
+            this.cb_producto3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProducto1 = new System.Windows.Forms.TextBox();
+            this.txtProducto2 = new System.Windows.Forms.TextBox();
+            this.txtProducto3 = new System.Windows.Forms.TextBox();
+            this.WASA = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_producto1
+            // 
+            this.cb_producto1.AutoSize = true;
+            this.cb_producto1.Location = new System.Drawing.Point(26, 96);
+            this.cb_producto1.Name = "cb_producto1";
+            this.cb_producto1.Size = new System.Drawing.Size(26, 19);
+            this.cb_producto1.TabIndex = 0;
+            this.cb_producto1.Text = "\r\n";
+            this.cb_producto1.UseVisualStyleBackColor = true;
+            // 
+            // cb_producto2
+            // 
+            this.cb_producto2.AutoSize = true;
+            this.cb_producto2.Location = new System.Drawing.Point(26, 205);
+            this.cb_producto2.Name = "cb_producto2";
+            this.cb_producto2.Size = new System.Drawing.Size(26, 19);
+            this.cb_producto2.TabIndex = 1;
+            this.cb_producto2.Text = "\r\n";
+            this.cb_producto2.UseVisualStyleBackColor = true;
+            // 
+            // cb_producto3
+            // 
+            this.cb_producto3.AutoSize = true;
+            this.cb_producto3.Location = new System.Drawing.Point(26, 325);
+            this.cb_producto3.Name = "cb_producto3";
+            this.cb_producto3.Size = new System.Drawing.Size(26, 19);
+            this.cb_producto3.TabIndex = 2;
+            this.cb_producto3.Text = "\r\n";
+            this.cb_producto3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(69, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 79);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(69, 178);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 79);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(69, 292);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 79);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.WASA);
+            this.panel1.Location = new System.Drawing.Point(69, 499);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 70);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(69, 431);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(512, 45);
+            this.btnTotal.TabIndex = 7;
+            this.btnTotal.Text = "Obtener Total de la Compra";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "1200";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "1500";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "2500";
+            // 
+            // txtProducto1
+            // 
+            this.txtProducto1.Location = new System.Drawing.Point(264, 94);
+            this.txtProducto1.Name = "txtProducto1";
+            this.txtProducto1.Size = new System.Drawing.Size(100, 23);
+            this.txtProducto1.TabIndex = 11;
+            // 
+            // txtProducto2
+            // 
+            this.txtProducto2.Location = new System.Drawing.Point(264, 203);
+            this.txtProducto2.Name = "txtProducto2";
+            this.txtProducto2.Size = new System.Drawing.Size(100, 23);
+            this.txtProducto2.TabIndex = 12;
+            // 
+            // txtProducto3
+            // 
+            this.txtProducto3.Location = new System.Drawing.Point(264, 322);
+            this.txtProducto3.Name = "txtProducto3";
+            this.txtProducto3.Size = new System.Drawing.Size(100, 23);
+            this.txtProducto3.TabIndex = 13;
+            // 
+            // WASA
+            // 
+            this.WASA.AutoSize = true;
+            this.WASA.Location = new System.Drawing.Point(26, 27);
+            this.WASA.Name = "WASA";
+            this.WASA.Size = new System.Drawing.Size(40, 15);
+            this.WASA.TabIndex = 0;
+            this.WASA.Text = "WASA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 608);
+            this.Controls.Add(this.txtProducto3);
+            this.Controls.Add(this.txtProducto2);
+            this.Controls.Add(this.txtProducto1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cb_producto3);
+            this.Controls.Add(this.cb_producto2);
+            this.Controls.Add(this.cb_producto1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cb_producto1;
+        private System.Windows.Forms.CheckBox cb_producto2;
+        private System.Windows.Forms.CheckBox cb_producto3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProducto1;
+        private System.Windows.Forms.TextBox txtProducto2;
+        private System.Windows.Forms.TextBox txtProducto3;
+        private System.Windows.Forms.Label WASA;
+    }
+}
+
---
+++ Pedro/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pedro
+{
+    public partial class Form1 : Form
+    {
+        int cantidad1, cantidad2, cantidad3;
+        int precio1, precio2, precio3;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible =false;
+            
+        }
+
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cb_producto1.Checked == true && txtProducto1.Text != "")
+                {
+                    cantidad1 = Convert.ToInt32(txtProducto1.Text);
+                    precio1 = cantidad1 * 1200;
+
+                }
+                else precio1 = 0;
+                if (cb_producto2.Checked == true && txtProducto2.Text != "")
+                {
+                    cantidad2 = Convert.ToInt32(txtProducto2.Text);
+                    precio2 = cantidad2 * 1500;
+
+                }
+                else precio2 = 0;
+                if (cb_producto3.Checked == true && txtProducto3.Text != "")
+                {
+                    cantidad3 = Convert.ToInt32(txtProducto3.Text);
+                    precio3 = cantidad3 * 2500;
+
+                }
+                else precio3 = 0;
+
+                int total = precio1 + precio2 + precio3;
+
+                panel1.Visible = true;
+
+                WASA.Text = Convert.ToString(total);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No hagas eso papu");
+            }
+           
+
+        }
+    }
+}
